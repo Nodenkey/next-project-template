44132683d18b001443f319024888893a5b11be78
Nii <52842378+Nodenkey@users.noreply.github.com>
2024-01-23 00:35:53 +0000

    Add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,8 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={inter.className} suppressHydrationWarning={true}>
-				{children}
+				{/*<Navbar />*/}
+				<main> {children} </main>
 			</body>
 		</html>
 	);
